c7c47f605307f27398f35e63e2a4cdd4cd8b7986
Ammar Nanjiani <ammar.nanjiani@gmail.com>
2024-10-08 10:57:37 -0500

    set up for templates

--- flake.nix
+++ flake.nix
@@ -1,50 +1,16 @@
 {
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    systems.url = "github:nix-systems/default";
-    devenv.url = "github:cachix/devenv";
-    devenv.inputs.nixpkgs.follows = "nixpkgs";
-  };
+  description = "Templates";
 
-  nixConfig = {
-    extra-trusted-public-keys =
-      "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
-    extra-substituters = "https://devenv.cachix.org";
-  };
+  outputs =
+    { ... }:
+    {
+      templates = {
+        python-data = {
+          path = ./python-data;
+          description = "Boilerplate for python data projects";
+        };
 
-  outputs = { self, nixpkgs, devenv, systems, ... }@inputs:
-    let forEachSystem = nixpkgs.lib.genAttrs (import systems);
-    in {
-      packages = forEachSystem (system: {
-        devenv-up = self.devShells.${system}.default.config.procfileScript;
-      });
-
-      devShells = forEachSystem (system:
-        let pkgs = nixpkgs.legacyPackages.${system};
-        in {
-          default = devenv.lib.mkShell {
-            inherit inputs pkgs;
-            modules = [{
-              # https://devenv.sh/reference/options/
-
-              packages = (with pkgs; [ ruff ])
-                ++ (with pkgs.python311Packages; [
-                  mypy
-                  debugpy
-                  python-lsp-server
-                  python-lsp-ruff
-                  pylsp-mypy
-                  pandas-stubs
-                ]);
-
-              languages.python = {
-                enable = true;
-                poetry.enable = true;
-                poetry.activate.enable = true;
-                poetry.install.enable = true;
-              };
-            }];
-          };
-        });
+      default = python-data;
+      };
     };
 }
