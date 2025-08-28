3426c72f618fbec4f3334717c2aa31af27a3a563
Ammar Nanjiani <ammar.nanjiani@gmail.com>
2025-08-28 13:16:55 -0500

    remove old template

--- flake.nix
+++ flake.nix
@@ -4,15 +4,9 @@
   outputs =
     { ... }:
     {
-      templates = {
-        python-data = {
-          path = ./python-data;
-          description = "Boilerplate for python data projects";
-        };
-        python-uv= {
-          path = ./python-uv;
-          description = "Boilerplate for python uv projects";
-        };
+      python-uv = {
+        path = ./python-uv;
+        description = "Boilerplate for python uv projects";
       };
     };
 }
