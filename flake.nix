18e27a0ae325e2b34383b1bbe9d3a2a3d129391d
Ammar Nanjiani <ammar.nanjiani@gmail.com>
2025-08-28 14:25:38 -0500

    flake-parts

--- flake.nix
+++ flake.nix
@@ -3,69 +3,61 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
     git-hooks = {
       url = "github:cachix/git-hooks.nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-  outputs =
-    {
-      self,
-      nixpkgs,
-      git-hooks,
-      ...
-    }:
-    let
 
-      # Support multiple systems
-      supportedSystems = [
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
         "x86_64-linux"
         "aarch64-linux"
         "x86_64-darwin"
         "aarch64-darwin"
       ];
-      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-    in
-    {
-      python-uv = {
-        path = ./python-uv;
-        description = "Boilerplate for python uv projects";
-      };
 
-      checks = forAllSystems (system: {
-        pre-commit-check = git-hooks.lib.${system}.run {
-          src = ./.;
-          hooks = {
-            # General file hygiene
-            trim-trailing-whitespace.enable = true;
-            end-of-file-fixer.enable = true;
-            check-merge-conflicts.enable = true;
-            check-added-large-files = {
-              enable = true;
-              args = [ "--maxkb=5000" ];
-            };
-            check-yaml.enable = true;
-            check-json.enable = true;
-            check-toml.enable = true;
-            flake-checker.enable = true;
-            # nix-fmt-rfc-style.enable = true;
-            deadnix.enable = true;
-            statix.enable = true;
+      flake = {
+        templates = {
+          python-uv = {
+            path = ./python-uv;
+            description = "Boilerplate for python uv projects";
           };
         };
-      });
+      };
 
-      devShells = forAllSystems (
-        system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-        in
-        {
-          default = pkgs.mkShell {
-            inherit (self.checks.${system}.pre-commit-check) shellHook;
-            buildInputs = self.checks.${system}.pre-commit-check.enabledPackages;
+      perSystem = { config, pkgs, system, ... }: {
+        checks = {
+          pre-commit-check = inputs.git-hooks.lib.${system}.run {
+            src = ./.;
+            hooks = {
+              # General file hygiene
+              trim-trailing-whitespace.enable = true;
+              end-of-file-fixer.enable = true;
+              check-merge-conflicts.enable = true;
+              check-added-large-files = {
+                enable = true;
+                args = [ "--maxkb=5000" ];
+              };
+              check-yaml.enable = true;
+              check-json.enable = true;
+              check-toml.enable = true;
+              flake-checker.enable = true;
+              # nix-fmt-rfc-style.enable = true;
+              deadnix.enable = true;
+              statix.enable = true;
+            };
           };
-        }
-      );
+        };
+
+        devShells = {
+          default = pkgs.mkShell {
+            inherit (config.checks.pre-commit-check) shellHook;
+            buildInputs = config.checks.pre-commit-check.enabledPackages;
+          };
+        };
+      };
     };
 }
